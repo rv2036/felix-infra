dbb9d33263838c2db31f58c1db67bd2b02627d28
Felix Yan <felixonmars@archlinux.org>
2022-01-20 03:05:48 +0200

    Add CI to validate gitakc.json

---
+++ .github/workflows/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Parsing gitakc.json"
+users="$(jq -r .userMap[][] gitakc.json)"
+
+for user in ${users[@]}; do
+    echo "Fetching keys for $user..."
+    keys=$(curl -fsS https://github.com/$user.keys)
+
+    echo "Checking if $user has SSH keys added..."
+    if [[ -z "${keys[@]}" ]]; then
+        echo "No SSH key found for $user."
+        exit 1
+    fi
+done
